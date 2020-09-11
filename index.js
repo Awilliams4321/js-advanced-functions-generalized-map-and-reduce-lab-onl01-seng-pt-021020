2dda1096b1282ed5f8b3a5b37a9d4faca9132470
Awilliams4321 <awilliams1234@live.com>
2020-09-11 13:30:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+
+function map(array, func) {
+    return array.map(value => func(value))
+}
+
+function reduce(array, func, start) {
+    let value = (!!start) ? start : array[0];
+    let i = (!!start) ? 0 : 1;
+
+    for (; i < array.length; i++) {
+        value = func(array[i], value)
+    }
+
+    return value;
+}
